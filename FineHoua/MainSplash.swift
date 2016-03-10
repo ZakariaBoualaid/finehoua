ae45302aebadd1155dd2ffdf59ee0aef31920220
Zakaria Boualaid <boulaidzac@gmail.com>
2016-03-10 23:19:40 +0000

    UITextField in the middle, Need button to go.

--- FineHoua/MainSplash.swift
+++ FineHoua/MainSplash.swift
@@ -15,39 +15,55 @@ class MainSplash: SKScene {
     var yourNameLabel:SKLabelNode!
     var fineHouaLabel:SKLabelNode!
     var refreshAlert:UIAlertController!
+    var xMid:CGFloat!
+    var yMid:CGFloat!
+    var xMax:CGFloat!
+    var yMax:CGFloat!
+
     
     override func didMoveToView(view: SKView) {
-        self.fineHouaLabel = SKLabelNode(fontNamed: "Chalkduster")
-        self.yourNameLabel = SKLabelNode(fontNamed: "Chalkduster")
-        
-        self.fineHouaLabel.fontSize = 66
-        self.yourNameLabel.fontSize = 22
-        
-        self.fineHouaLabel.color = GameViewController.colorWithHexString("#ffffff")
-        self.yourNameLabel.color = GameViewController.colorWithHexString("#ffffff")
-        
-        self.fineHouaLabel.text = "Fine Houa?"
-        self.yourNameLabel.text = "Your name"
-        
-        self.fineHouaLabel.name = "fineHouaLabel"
-        self.yourNameLabel.name = "yourNameLabel"
-        
-        self.fineHouaLabel.position = CGPoint(x: CGRectGetMidX(self.scene!.frame), y: CGRectGetMidY(self.scene!.frame))
-        self.yourNameLabel.position = CGPoint(x: CGRectGetMidX(self.scene!.frame), y: CGRectGetMidY(self.scene!.frame) - fineHouaLabel.frame.height)
-        
-        self.addChild(self.fineHouaLabel)
-        self.addChild(self.yourNameLabel)
+        initVars()
+        addControls()
     }
     
     private func addControls(){
+        // Game Title & Label
+        self.fineHouaLabel = SKLabelNode(fontNamed: "Chalkduster")
+        self.yourNameLabel = SKLabelNode(fontNamed: "Chalkduster")
+        self.fineHouaLabel.fontSize = 66
+        self.yourNameLabel.fontSize = 22
+        self.fineHouaLabel.color = GameViewController.colorWithHexString("#ffffff")
+        self.yourNameLabel.color = GameViewController.colorWithHexString("#ffffff")
+        self.fineHouaLabel.text = "Fine Houa?"
+        self.yourNameLabel.text = "Your name"
+        self.fineHouaLabel.name = "fineHouaLabel"
+        self.yourNameLabel.name = "yourNameLabel"
+        self.fineHouaLabel.position = CGPoint(x: self.xMid, y: self.yMax - 100.00)
+        self.yourNameLabel.position = CGPoint(x: self.xMid, y: self.yMid - 50.00)
+        self.addChild(self.fineHouaLabel)
+        // self.addChild(self.yourNameLabel)
         
-
+        self.nameTextField = UITextField(frame: CGRect(x: self.xMid - 175.00, y: self.yMid - 50, width: 350.00, height: 55.00))
+        self.nameTextField.attributedPlaceholder = NSAttributedString(string: "Your name", attributes: [NSForegroundColorAttributeName: UIColor.whiteColor()])
+        self.nameTextField.textColor = UIColor.whiteColor()
+        self.nameTextField.font = UIFont(name: "Chalkduster", size: 24.00)
+        self.nameTextField.backgroundColor = GameViewController.colorWithHexString("#232323")
+        self.nameTextField.tintColor = GameViewController.colorWithHexString("#ffffff")
+        self.nameTextField.textAlignment = .Center
+        self.view!.addSubview(nameTextField)
     }
     
     private func startGame(){
         let gameScene = GameScene(size: view!.bounds.size)
         let transition = SKTransition.fadeWithDuration(0.15)
         view!.presentScene(gameScene, transition: transition)
+    }
+    
+    private func initVars(){
+        self.xMid = CGRectGetMidX(self.scene!.frame)
+        self.yMid = CGRectGetMidY(self.scene!.frame)
+        self.xMax = CGRectGetMaxX(self.scene!.frame)
+        self.yMax = CGRectGetMaxY(self.scene!.frame)
     }
 
 }
